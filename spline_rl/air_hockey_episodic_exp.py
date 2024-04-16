9070a7b433d34c9c61b3221dc40f61c037418cf3
pkicki <piotr.m.kicki@gmail.com>
2024-04-16 08:28:58 +0000

    Seed as argument

--- spline_rl/air_hockey_episodic_exp.py
+++ spline_rl/air_hockey_episodic_exp.py
@@ -51,6 +51,8 @@ def experiment(env: str = 'air_hockey',
                render: bool = False,
                results_dir: str = './logs',
                **kwargs):
+    if len(sys.argv) > 1:
+        seed = int(sys.argv[1])
     np.random.seed(seed)
     torch.manual_seed(seed)
 
