93c4abae3b83df0aac626592927ded85c3908651
pkicki <piotr.m.kicki@gmail.com>
2024-07-25 23:58:12 +0200

    Box pushing launch

--- experiments/launch_bsmp.py
+++ experiments/launch_bsmp.py
@@ -19,12 +19,14 @@ PARTITION = 'standard'  # 'amd', 'rtx'
 GRES = 'gpu:1' if USE_CUDA else None  # gpu:rtx2080:1, gpu:rtx3080:1
 CONDA_ENV = None
 
-experiment_name = 'bsmp_eppo_kinodynamic'
+#experiment_name = 'bsmp_eppo_kinodynamic'
+experiment_name = 'bsmp_eppo_box_pushing'
 
 launcher = Launcher(
     exp_name=experiment_name,
     #exp_file='air_hockey_episodic_exp',
-    exp_file='kinodynamic_cup_episodic_exp',
+    #exp_file='kinodynamic_cup_episodic_exp',
+    exp_file='box_pushing_episodic_exp',
     # project_name='project01234',  # for hrz cluster
     n_seeds=N_SEEDS,
     n_exps_in_parallel=N_EXPS_IN_PARALLEL,
@@ -43,14 +45,15 @@ launcher = Launcher(
 
 launcher.add_experiment(
     #alg="bsmp_eppo_stop",
-    alg="bsmp_eppo_kinodynamic",
+    #alg="bsmp_eppo_kinodynamic",
+    alg="bsmp_eppo_box_pushing",
     group_name=experiment_name,
 
-    # kinodynamic
-    initial_entropy_lb = 45,
-    entropy_lb = -45 / 2.,
-    q_d_scale = 1. / 150., # structured
-    #q_d_scale = 1. / 50., # unstructured
+    ## kinodynamic
+    #initial_entropy_lb = 45,
+    #entropy_lb = -45 / 2.,
+    #q_d_scale = 1. / 150., # structured
+    ##q_d_scale = 1. / 50., # unstructured
 
 
     # air hockey
