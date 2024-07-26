Fixed duration scaling issues

spline_rl/policy/bsmp_policy.py
@@ -276,9 +276,14 @@ class BSMPPolicy(Policy):
         episode_duration = 2.
         duration = torch.tile(duration[:, None, None], (1, dtau_dt.shape[1], 1))
         too_long_trajectory = duration > episode_duration
-        dtau_dt_ = dtau_dt * duration / episode_duration
-        s = torch.sum(ddtau_dtt / dtau_dt**2, axis=-2, keepdims=True) / dtau_dt.shape[-2]
-        ddtau_dtt_ = (ddtau_dtt * duration - dtau_dt * s) / episode_duration
+        c = duration / episode_duration
+        log_c = torch.log(c)
+        tN_inv = torch.linalg.pinv(tN)
+        t_cps_update = tN_inv @ log_c
+        t_cps_ = t_cps + t_cps_update
+        dtau_dt_ = torch.exp(tN @ t_cps_) if differentiable else np.exp(tN @ t_cps_)
+        ddtau_dtt_ = dtau_dt_**2 * (tdN @ t_cps_)
+
         dtau_dt = torch.where(too_long_trajectory, dtau_dt_, dtau_dt)
         ddtau_dtt = torch.where(too_long_trajectory, ddtau_dtt_, ddtau_dtt)
         #t_, dt_, duration_ = compute_duration(dtau_dt_)